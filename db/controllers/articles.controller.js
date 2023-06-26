a40d786f3b22f81e0123639260ce7c5a75534848
adamsaunders26 <a.saunders206@gmail.com>
2023-06-26 15:02:11 +0100

    removed console.log

--- db/controllers/articles.controller.js
+++ db/controllers/articles.controller.js
@@ -4,10 +4,7 @@ exports.getArticle = (req, res, next) => {
   const { article_id } = req.params;
   selectArticle(article_id)
     .then((article) => {
-      console.log(article);
       res.status(200).send({ article });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch(next);
 };
