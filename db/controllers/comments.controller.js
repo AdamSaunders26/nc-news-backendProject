2dceef0a88f63f254ee6f753db1740786862a35c
adamsaunders26 <a.saunders206@gmail.com>
2023-06-27 14:43:04 +0100

    fix some weird bugs that I don't fully understand

--- db/controllers/comments.controller.js
+++ db/controllers/comments.controller.js
@@ -3,6 +3,9 @@ const { selectComments, commentChecker } = require("../models/comments.model");
 exports.getComments = (req, res, next) => {
   const { article_id } = req.params;
 
+  if (/[A-z]/gi.test(article_id)) {
+    next({ status: 400, message: "Error: Bad Request" });
+  }
   Promise.all([commentChecker(article_id), selectComments(article_id)])
     .then((resolvedPromises) => {
       res.status(200).send({ comments: resolvedPromises[1] });
