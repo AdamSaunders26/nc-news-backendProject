650c263443b7fafefbe63ac4e1fcc1d5f8cc27a6
adamsaunders26 <a.saunders206@gmail.com>
2023-07-10 20:24:43 +0100

    add POST /api/topics and tests

--- db/models/topics.model.js
+++ db/models/topics.model.js
@@ -2,6 +2,17 @@ const db = require("../connection");
 
 exports.selectTopics = () => {
   return db.query("SELECT * FROM topics").then(({ rows }) => {
-    return rows
+    return rows;
   });
+};
+
+exports.insertTopic = (topicToPost) => {
+  return db
+    .query(
+      `INSERT INTO topics (slug, description) VALUES ($1, $2) RETURNING *`,
+      [topicToPost.slug, topicToPost.description]
+    )
+    .then((postedTopic) => {
+      return postedTopic.rows[0];
+    });
 };
